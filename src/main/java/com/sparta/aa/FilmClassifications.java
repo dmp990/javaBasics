9696965c36d18cd4ac104b478b8f3b69d024862e
Asad Ali <asadali.asif.1@gmail.com>
2023-05-02 15:27:02 +0100

    Add Program and Test files

---
+++ src/main/java/com/sparta/aa/FilmClassifications.java
@@ -0,0 +1,4 @@
+package com.sparta.aa;
+
+public class FilmClassifications {
+}
--- src/test/java/com/sparta/aa/ProgramTests.java
+++ src/test/java/com/sparta/aa/ProgramTests.java
@@ -1,4 +1,18 @@
 package com.sparta.aa;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class ProgramTests {
+
+    // annotations carry metadata
+    @ParameterizedTest
+    @ValueSource(ints = {5, 8, 12})
+    @DisplayName("check that 5, 8, 12 returns Good Morning")
+    void checkForGoodMorning(int time) {
+        Assertions.assertEquals("Good morning!", Program.getGreeting(time));
+    }
 }

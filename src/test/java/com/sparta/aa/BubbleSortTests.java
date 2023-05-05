b7c6f79365565247c1d4265b597f871d28b11917
Asad Ali <asadali.asif.1@gmail.com>
2023-05-05 09:20:57 +0100

    Add test case

--- src/test/java/com/sparta/aa/BubbleSortTests.java
+++ src/test/java/com/sparta/aa/BubbleSortTests.java
@@ -10,8 +10,17 @@ public class BubbleSortTests {
     int[] sorted = {7, 10, 11, 14, 16, 18, 25, 28, 30, 50};
 
     @Test
-    @DisplayName("check if the array has properly been sorted in ascending order")
-    void checkForPalindromes() {
+    @DisplayName("check if the array of positive values has properly been sorted in ascending order")
+    void checkForPositiveArray() {
+
+        BubbleSort.bubbleSort(arr);
+        Assertions.assertArrayEquals(sorted, arr);
+    }
+    @Test
+    @DisplayName("check if the array with some negative values has properly been sorted in ascending order")
+    void checkForArrayWithNegativeValues() {
+        int[] arr = {-1, 25, -4, 3, 44};
+        int[] sorted = {-4, -1, 3, 25, 44};
         BubbleSort.bubbleSort(arr);
         Assertions.assertArrayEquals(sorted, arr);
     }
